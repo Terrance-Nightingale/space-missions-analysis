30700d5fb3a95d2917a4c1e57e918fd5b5a5e9a9
Terrance-Nightingale <130939937+Terrance-Nightingale@users.noreply.github.com>
2023-10-20 11:08:55 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# TODO 1) Import the space mission data
+# TODO 2) Determine the data structure
+# TODO 3) Based on data structure, determine best questions to ask
+# TODO 4) Based on questions, determine best graphic representation method (pie chart, bar graph, etc.)
